efa77589f225726b52241b8ad4eecd38fa11ad2c
Michel Novus <michelnovus@gmail.com>
2024-08-12 19:25:11 -0300

    A small argument parser and installers for some shells.

--- prettyline/src/main.rs
+++ prettyline/src/main.rs
@@ -1,3 +1,49 @@
 // [MIT License] Copyright (c) 2024 Michel Novus
 
-fn main() {}
+use anyhow::{anyhow, Result};
+use clap::Parser;
+
+fn main() -> Result<()> {
+    let args = setup::Args::parse();
+
+    Ok(())
+}
+
+pub mod setup {
+    use clap::{Parser, ValueEnum};
+
+    #[derive(Debug, Parser)]
+    pub struct Args {
+        /// Sets shell settings.
+        #[arg(long, value_name = "SHELL")]
+        pub init: Option<ShellName>,
+    }
+
+    #[derive(Debug, Clone, Copy, ValueEnum)]
+    pub enum ShellName {
+        Bash,
+        Zsh,
+        Fish,
+    }
+
+    pub mod install {
+        pub fn bash() {
+            unimplemented!()
+        }
+        pub fn zsh() {
+            unimplemented!()
+        }
+        pub fn fish() {
+            let script = "\
+            function fish_prompt\n    \
+                    command prettyline\n\
+                end\n\
+                function fish_right_prompt\n    \
+                command prettyline\n\
+                end\n
+                ";
+            println!("{}", script);
+            unimplemented!();
+        }
+    }
+}
