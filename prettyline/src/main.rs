6d4b04fd58893ce44674a8f58f6c519232e1a059
Michel Novus <michelnovus@gmail.com>
2024-08-12 22:55:50 -0300

    Many things were added.

--- prettyline/src/main.rs
+++ prettyline/src/main.rs
@@ -2,6 +2,8 @@
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
+use constants::{colors, symbols};
+use std::env;
 
 fn main() -> Result<()> {
     let args = setup::Args::parse();
@@ -23,7 +25,87 @@ fn main() -> Result<()> {
         };
     }
 
+    let mut left_prompt: Vec<prompt::Segment> = vec![];
+
+    let username = match env::var_os("USER")
+        .unwrap_or_else(|| "???".into())
+        .into_string()
+    {
+        Ok(val) => val,
+        Err(_) => {
+            return Err(anyhow!(
+                "It seems that the username has strange characters"
+            ))
+        }
+    };
+    let user_segment = prompt::Segment {
+        left: None,
+        center: prompt::Chunk::new(&username)
+            .fg(colors::USER_NORM_FG)
+            .bg(colors::USER_NORM_BG)
+            .weight(prompt::TextWeight::Bold)
+            .pad(),
+        right: Some(
+            prompt::Chunk::new(symbols::R_ANGLED_FILL).fg(colors::USER_NORM_BG),
+        ),
+    };
+    left_prompt.push(user_segment);
+
+    let git_segment = prompt::Segment {
+        left: None,
+        center: prompt::Chunk::new(""),
+        right: None,
+    };
+    left_prompt.push(git_segment);
+
+    let exit_status = match args.exit_status {
+        Some(value) => format!("E{}", value),
+        None => "E?".into(),
+    };
+    let exitcode_segment = prompt::Segment {
+        left: None,
+        center: prompt::Chunk::new(&exit_status),
+        right: None,
+    };
+    left_prompt.push(exitcode_segment);
+
+    println!("{:#?}", left_prompt);
+
     Ok(())
+}
+
+pub mod constants {
+    pub mod symbols {
+        pub const R_ANGLED_FILL: &'static str = "\u{E0B0}"; // 
+        pub const L_ANGLED_FILL: &'static str = "\u{E0B2}"; // 
+        pub const R_ANGLED_FLAT: &'static str = "\u{E0B1}"; // 
+        pub const L_ANGLED_FLAT: &'static str = "\u{E0B3}"; // 
+        pub const R_CURVED_FILL: &'static str = "\u{E0B4}"; // 
+        pub const L_CURVED_FILL: &'static str = "\u{E0B6}"; // 
+        pub const R_CURVED_FLAT: &'static str = "\u{E0B5}"; // 
+        pub const L_CURVED_FLAT: &'static str = "\u{E0B7}"; // 
+        pub const HONEYCOMB_FILL: &'static str = "\u{E0CC}"; // 
+        pub const HONEYCOMB_FLAT: &'static str = "\u{E0CD}"; // 
+        pub const BRANCH: &'static str = "\u{E0A0}"; // 
+    }
+    pub mod colors {
+        use anstyle::{AnsiColor, Color};
+        pub const USER_NORM_FG: Color = Color::Ansi(AnsiColor::Black);
+        pub const USER_NORM_BG: Color = Color::Ansi(AnsiColor::White);
+        pub const USER_SUDO_FG: Color = Color::Ansi(AnsiColor::Black);
+        pub const USER_SUDO_BG: Color = Color::Ansi(AnsiColor::BrightYellow);
+        pub const USER_ROOT_FG: Color = Color::Ansi(AnsiColor::Black);
+        pub const USER_ROOT_BG: Color = Color::Ansi(AnsiColor::BrightMagenta);
+
+        pub const EXITCODE_SUCCESS_FG: Color = Color::Ansi(AnsiColor::Black);
+        pub const EXITCODE_SUCCESS_BG: Color =
+            Color::Ansi(AnsiColor::BrightBlue);
+        pub const EXITCODE_FAILED_FG: Color = Color::Ansi(AnsiColor::Black);
+        pub const EXITCODE_FAILED_BG: Color = Color::Ansi(AnsiColor::BrightRed);
+
+        pub const TIME_FG: Color = Color::Ansi(AnsiColor::Black);
+        pub const TIME_BG: Color = Color::Ansi(AnsiColor::White);
+    }
 }
 
 pub mod setup {
@@ -73,6 +155,91 @@ pub mod setup {
             end\
             ";
             println!("{}", script);
+        }
+    }
+}
+
+pub mod prompt {
+    use anstyle::{Color, Style};
+    use std::fmt::Display;
+
+    #[derive(Debug)]
+    pub struct Segment<'a> {
+        pub left: Option<Chunk<'a>>,
+        pub center: Chunk<'a>,
+        pub right: Option<Chunk<'a>>,
+    }
+    impl<'a> Display for Segment<'a> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let left: String = match self.left.as_ref() {
+                Some(chunk) => chunk.to_string(),
+                None => String::new(),
+            };
+            let center: String = self.center.to_string();
+            let right: String = match self.right.as_ref() {
+                Some(chunk) => chunk.to_string(),
+                None => String::new(),
+            };
+            write!(f, "{}{}{}", left, center, right)
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum TextWeight {
+        Bold,
+        Dimm,
+    }
+
+    #[derive(Debug, Default)]
+    pub struct Chunk<'a> {
+        pub value: &'a str,
+        pub weight: Option<TextWeight>,
+        pub fg_color: Option<Color>,
+        pub bg_color: Option<Color>,
+        pad: bool,
+    }
+    impl<'a> Chunk<'a> {
+        pub fn new(text: &'a str) -> Self {
+            Self {
+                value: text,
+                weight: None,
+                fg_color: None,
+                bg_color: None,
+                pad: false,
+            }
+        }
+        pub fn fg(mut self, color: Color) -> Self {
+            self.fg_color = Some(color);
+            self
+        }
+        pub fn bg(mut self, color: Color) -> Self {
+            self.bg_color = Some(color);
+            self
+        }
+        pub fn weight(mut self, weight: TextWeight) -> Self {
+            self.weight = Some(weight);
+            self
+        }
+        pub fn pad(mut self) -> Self {
+            self.pad = !self.pad;
+            self
+        }
+    }
+    impl<'a> Display for Chunk<'a> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let mut style =
+                Style::new().fg_color(self.fg_color).bg_color(self.bg_color);
+            if let Some(weight) = &self.weight {
+                style = match weight {
+                    TextWeight::Bold => style.bold(),
+                    TextWeight::Dimm => style.dimmed(),
+                }
+            }
+            if self.pad {
+                write!(f, "{style} {} {style:#}", self.value)
+            } else {
+                write!(f, "{style}{}{style:#}", self.value)
+            }
         }
     }
 }
